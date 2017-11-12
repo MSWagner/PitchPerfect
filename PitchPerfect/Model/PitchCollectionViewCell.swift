e651119fe7293655a5e46eb7daab56a54a288ff5
Matthias Wagner <24782257+MSWagner@users.noreply.github.com>
2017-11-12 20:57:08 +0100

    Upload Project with UI implementation

---
+++ PitchPerfect/Model/PitchCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PitchCollectionViewCell.swift
+//  PitchPerfect
+//
+//  Created by Matthias Wagner on 12.11.17.
+//  Copyright © 2017 Michael Wagner. All rights reserved.
+//
+
+import UIKit
+
+class PitchCollectionViewCell: UICollectionViewCell {
+    
+}
